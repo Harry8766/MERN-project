ee7a584e536ca022089995a55a9d0c6adec356bf
HARRY <harshkumarjha296@gmail.com>
2025-08-19 20:31:04 +0530

    add to cart work started

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext,useState } from "react";
+import { createContext,useState,useEffect } from "react";
 import {products} from '../assets/assets';
 
 
@@ -11,10 +11,39 @@ import {products} from '../assets/assets';
         const delivery_fee=10;
         const [search,setSearch] = useState('');
         const[showSearch,setShowSearch] = useState(false);
+        const[cartItems,setCartItems] = useState({});
+
+        const addToCart = async(itemId,size)=>{
+
+            let cartData = structuredClone(cartItems);
+
+            if(cartData[itemId]){
+
+                if(cartData[itemId][size]){
+                    cartData[itemId][size] += 1;
+
+        }
+                else{
+                    cartData[itemId][size] = 1;
+                }
+                
+                
+                }
+                else{
+                    cartData[itemId] = {};
+                    cartData[itemId][size] = 1;
+                }
+                setCartItems(cartData);
+            }
+
+            useEffect(()=>{
+                console.log(cartItems);
+            },[cartItems])
 
     const value = {
             products,currency,
-            delivery_fee,search,setSearch,showSearch,setShowSearch
+            delivery_fee,search,setSearch,showSearch,setShowSearch,
+            cartItems,addToCart
     }
     return(
         <ShopContext.Provider value={value}>
